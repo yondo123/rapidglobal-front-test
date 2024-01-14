6bdd9718a4379cdc8d454ea066118310a759ee52
yondo123 <46988995+yondo123@users.noreply.github.com>
2024-01-14 11:35:39 +0900

    chore: 테스트 환경 구성

---
+++ jest.config.ts
@@ -0,0 +1,32 @@
+import nextJest from 'next/jest';
+import type { Config } from 'jest';
+
+const nextJestConfig = nextJest({
+  dir: './'
+});
+
+const config: Config = {
+  verbose: true,
+  rootDir: './',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  projects: ['<rootDir>/jest.config.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/.swc'],
+  testEnvironment: 'jest-environment-jsdom',
+  testEnvironmentOptions: {
+    url: 'https://localhost:3000'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    '!./src/**/_*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@product/(.*)$': '<rootDir>/src/product/$1'
+  }
+};
+
+export default nextJestConfig(config);
