5e0ca70437f5f49ad20ae30da512e7e8925dd9b4
yondo123 <46988995+yondo123@users.noreply.github.com>
2024-01-15 09:39:00 +0900

    test: 불필요 테스트 코드 삭제

--- src/layouts/tests/Button.test.tsx
+++ src/layouts/tests/Button.test.tsx
@@ -11,13 +11,6 @@ const renderButton = (props?: any) =>
     </EmotionThemeProviderMock>
   );
 describe('Button UI 컴포넌트 테스트', () => {
-  it('Button은 정의된 ColorScheme에 따라 여러 색상을 갖는다.', async () => {
-    const handleClick = jest.fn();
-    renderButton({ onClick: handleClick });
-    const $button = screen.getByRole('button');
-    await userEvent.click($button);
-    expect(handleClick).toHaveBeenCalled();
-  });
   it('Button을 클릭하면 클릭 이벤트가 실행된다.', async () => {
     const handleClick = jest.fn();
     renderButton({ onClick: handleClick });
@@ -34,7 +27,7 @@ describe('Button UI 컴포넌트 테스트', () => {
     expect(handleClick).toHaveBeenCalledTimes(0);
   });
 
-  it.each(MOCK_BUTTONS_SIZE)('Button은 크기에 따라 다른 높이를 가진다. ($size)', ({ size, height }) => {
+  it.each(MOCK_BUTTONS_SIZE)('Button은 크기에 따라 다른 높이를 갖는다. ($size)', ({ size, height }) => {
     renderButton({ size });
     const $button = screen.getByRole('button');
     expect($button).toHaveStyle({ height });
