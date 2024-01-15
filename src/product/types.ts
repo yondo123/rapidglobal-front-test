4c5d6052eeb7c5d58b3ffce24b7a5a224649744b
yondo123 <46988995+yondo123@users.noreply.github.com>
2024-01-15 20:17:42 +0900

    feat: 제품 타입 정의

---
+++ src/product/types.ts
@@ -0,0 +1,28 @@
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  uploadedAt: string;
+  viewCount: number;
+  thumbnailUrls: string[];
+};
+
+export type SortAction = 'asc' | 'desc' | null;
+
+export type SortList = {
+  price?: SortAction;
+  productTitle?: SortAction;
+  uploadedAt?: SortAction;
+}[];
+
+export type ProductSort = {
+  priceSort: SortAction;
+  titleSort: SortAction;
+  uploadedAtSort: SortAction;
+};
+
+export type RequestProductsParams = {
+  skip: number;
+  take: number;
+  sortList?: SortList;
+};
