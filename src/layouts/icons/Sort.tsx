41026113ad65d45acec8075ebf43cd027fe7b81b
yondo123 <46988995+yondo123@users.noreply.github.com>
2024-01-15 18:09:59 +0900

    ui: SVG 아이콘 파일 추가

---
+++ src/layouts/icons/Sort.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export const Sort = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 12 11" {...props}>
+    <path stroke="#9D86FF" strokeLinecap="round" strokeWidth="0.625" d="M1.125 3H5.5M1.125 5.5H5.5M1.125 8H5.5" />
+    <path
+      stroke="#9D86FF"
+      strokeWidth="0.625"
+      d="M9.875 10.5l1.875-1.875M9.875 10.5L8 8.625M9.875 10.5V.5m0 0L8 2.375M9.875.5l1.875 1.875"
+    />
+  </svg>
+);
