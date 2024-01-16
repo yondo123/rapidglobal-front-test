b157481296bcb75348054625b02ba45e1c9f0645
yondo123 <46988995+yondo123@users.noreply.github.com>
2024-01-17 06:38:49 +0900

    chore: 이미지 파일 수신을 위한 host 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'cdn.heyseller.kr' }]
+  }
 };
 
 module.exports = nextConfig;
