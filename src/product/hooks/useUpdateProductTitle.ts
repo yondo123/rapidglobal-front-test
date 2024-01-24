848574b15aa351ac25e02a4062cd1749712e9ec2
yondo123 <46988995+yondo123@users.noreply.github.com>
2024-01-24 23:55:34 +0900

    refactor: API 요청시 필요한 type은 model 계층으로 분리

--- src/product/hooks/useUpdateProductTitle.ts
+++ src/product/hooks/useUpdateProductTitle.ts
@@ -1,9 +1,13 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import type { MutateOptions } from '@tanstack/react-query';
 import { updateProductTitle } from '../api/updateProductTitle';
-import type { RequestProductUpdateParams } from '../types';
+import type { RequestProductUpdateParams } from '../api/models/product';
 
-export const useUpdateProductTitle = (params: RequestProductUpdateParams, options?: MutateOptions) => {
+export const useUpdateProductTitle = (
+  params: RequestProductUpdateParams,
+  options?: MutateOptions,
+  onSuccess?: () => void
+) => {
   const queryClient = useQueryClient();
   const { mutate, isPending } = useMutation({
     mutationFn: () => updateProductTitle(params),
@@ -11,6 +15,7 @@ export const useUpdateProductTitle = (params: RequestProductUpdateParams, option
       queryClient.invalidateQueries({
         queryKey: ['products']
       });
+      onSuccess?.();
     },
     ...options
   });
